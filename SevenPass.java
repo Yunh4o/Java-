ad286853901268e7ac4b18a776d5651d26ca1300
Yunh4o <48115850+Yunh4o@users.noreply.github.com>
2019-04-01 08:59:09 +0800

    Update SevenPass.java

--- SevenPass.java
+++ SevenPass.java
@@ -5,7 +5,7 @@ public class SevenPass {
 		
 		int sum=0;
 		for(int i=1;i<=100;i++) {
-			//不能输出100以内7的倍数和含有7的数
+			//输出100以内的不是7的倍数和不含有7的数
 			if(!(i%7==0 || i%10==7 || i/10==7)) {
 				System.out.print(i+"\t");
 				sum++;
